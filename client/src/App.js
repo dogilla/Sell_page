7ed8d0e54829d8f87531a8eca742d5cde0484e97
dogilla <19thmagm@ciencias.unam.mx>
2020-01-20 21:13:59 -0600

    navigation bar with react-bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,16 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Customers from './components/customers';
+import Navigationbar from './components/navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 
 class App extends Component {
   render() {
     return (
+      
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">React Express Start</h1>
-        </header>
+        <Navigationbar />
         <Customers />
         <Button bsStyle="primary"> primary </Button>
       </div>
